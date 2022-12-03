daed3100ded58ee2a3571b425e6626756de74448
Maksym Rohachov <romax982@gmail.com>
2022-12-03 10:06:30 +0100

    Add first algo tasks

---
+++ src/main/java/com/task/algorithm/TwoSum.java
@@ -0,0 +1,32 @@
+package com.task.algorithm;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * @author invzbl3 on 12/3/2022
+ * @project LeetCodeTask
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+
+            Integer diff = target - numbers[i];
+            if (hash.containsKey(diff)) {
+                return new int[]{hash.get(diff) + 1, i + 1};
+            }
+            hash.put(numbers[i], i);
+        }
+        return new int[0];
+    }
+}
