5e2666344b7a851e297f65c4c97245e0200acd20
Maksym Rohachov <romax982@gmail.com>
2022-12-10 14:46:27 +0100

    Update algorithhms

--- src/main/java/com/task/algorithm/Easy/solved/ReverseALinkedList.java
+++ src/main/java/com/task/algorithm/Easy/solved/ReverseALinkedList.java
@@ -9,13 +9,82 @@ import java.util.*;
  */
 
 /*
-    Algorithm description:
-    ---------------------
+    206. Reverse Linked List
+
+    Given the head of a singly linked list, reverse the list,
+    and return the reversed list.
+
+    Example 1:
+
+    Input: head = [1,2,3,4,5]
+    Output: [5,4,3,2,1]
+
+    Example 2:
+
+    Input: head = [1,2]
+    Output: [2,1]
+
+    Example 3:
+
+    Input: head = []
+    Output: []
+
+    Constraints:
+
+    * The number of nodes in the list is the range
+    [0, 5000].
+    * -5000 <= Node.val <= 5000
 
 
+    Follow up: A linked list can be reversed either
+    iteratively or recursively. Could you implement both?
  */
 
-public class ReverseALinkedList {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() { }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution2 {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next_node = head.next;
+            head.next = prev;
+            prev = head;
+            head = next_node;
+        }
+
+        return prev;
+    }
+}
+
+/*public class ReverseALinkedList {
     public static void main(String[] args) {
         // Declaring linkedlist without any initial size
         LinkedList<String> linkedli = new LinkedList<>();
@@ -39,7 +108,7 @@ public class ReverseALinkedList {
         // Return the reversed arraylist
         return revLinkedList;
     }
-}
+}*/
 
 /*
 class MyLinkedList {
---
+++ src/main/java/com/task/algorithm/Easy/solved/ValidAnagram.java
@@ -0,0 +1,130 @@
+package com.task.algorithm.Easy.solved;
+
+import java.util.Arrays;
+
+/**
+ * @author invzbl3 on 12/4/2022
+ * @project LeetCodeTask
+ */
+
+/*
+    Algorithm description:
+    ---------------------
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+    typically using all the original letters exactly once.
+
+    Example 1:
+
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+    Example 2:
+
+    Input: s = "rat", t = "car"
+    Output: false
+ */
+
+// https://www.programcreek.com/2014/05/leetcode-valid-anagram-java/
+// https://leetcode.com/problems/valid-anagram/
+
+class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] char_counts = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char_counts[s.charAt(i) - 'a']++;
+            char_counts[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : char_counts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/*class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] char_counts = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char_counts[s.charAt(i) - 'a']++;
+            char_counts[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : char_counts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}*/
+
+/*class Solution2 {
+    public static boolean isAnagram(String st1, String st2) {
+        String s1 = st1.replaceAll("\\s", "");
+        String s2 = st2.replaceAll("\\s", "");
+        boolean status;
+        if (s1.length() != s2.length()) {
+            status = false;
+        } else {
+            char[] arrays1 = s1.toLowerCase().toCharArray();
+            char[] arrays2 = s2.toLowerCase().toCharArray();
+
+            Arrays.sort(arrays1);
+            Arrays.sort(arrays2);
+            status = Arrays.equals(arrays1, arrays2);
+        }
+
+        if (status) {
+            System.out.println(s1 + ""
+                    + " and " + s2 + " are anagrams");
+        } else {
+            System.out.println(s1 + " and " + s2 + " are not anagrams");
+        }
+    }
+
+    public static void main(String[] args) {
+        isAnagram("keep", "peek");
+        isAnagram("MotherInLaw", "HitlerWoman");
+    }
+}*/
+
+/*
+public class ValidAnagram {
+
+    public static boolean solution(String s1, String s2) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            char ch = s1.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            char ch = s2.charAt(i);
+
+            if (!map.containsKey(ch)) {
+                return false;
+            } else if (map.get(ch) == 1) {
+                map.remove(ch);
+            } else {
+                map.put(ch, map.get(ch) - 1);
+            }
+        }
+        return map.size() == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String s1 = scn.next();
+        String s2 = scn.next();
+        System.out.println(solution(s1, s2));
+    }
+}*/
