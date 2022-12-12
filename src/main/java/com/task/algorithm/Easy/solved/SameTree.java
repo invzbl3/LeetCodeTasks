c8901c37ccfaa80ba6de7345800e51d48b1bb6a9
Maksym Rohachov <romax982@gmail.com>
2022-12-13 00:32:29 +0100

    Update description for algorithms

--- src/main/java/com/task/algorithm/Easy/solved/SameTree.java
+++ src/main/java/com/task/algorithm/Easy/solved/SameTree.java
@@ -1,27 +1,57 @@
 package com.task.algorithm.Easy.solved;
 
 /*
-    Algorithm description:
-    ---------------------
-    Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
-
-    For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
-
-        1
-       / \
-      2   2
-     / \ / \
-    3  4 4  3
-
-    But the following [1, 2, 2, null, 3, null, 3] is not:
-
-        1
-       / \
-      2   2
-      \   \
-      3    3
-
-    Note: Bonus points if you could solve it both recursively and iteratively.
+ * 100. Same Tree
+ * https://leetcode.com/problems/same-tree/
+ *
+ * Given the roots of two binary trees p and q, write a function to check if they are the same or
+ * not.
+ *
+ * Two binary trees are considered the same if they are structurally identical, and the nodes have
+ * the same value.
+ *
+ * Example 1:
+ *
+ * Input: p = [1,2,3], q = [1,2,3]
+ * Output: true
+ *
+ * Example 2:
+ *
+ * Input: p = [1,2], q = [1,null,2]
+ * Output: false
+ *
+ * Example 3:
+ *
+ * Input: p = [1,2,1], q = [1,1,2]
+ * Output: false
+ *
+ * Constraints:
+ *
+ * The number of nodes in both trees is in the range [0, 100].
+ * -10^4 <= Node.val <= 10^4
+ *
+ *
+ * An alternative algorithm description:
+ * -------------------------------------
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ *
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ *
+ *       1
+ *      / \
+ *     2   2
+ *    / \ / \
+ *   3  4 4  3
+ *
+ * But the following [1, 2, 2, null, 3, null, 3] is not:
+ *
+ *       1
+ *      / \
+ *     2   2
+ *     \   \
+ *     3    3
+ *
+ * Note: Bonus points if you could solve it both recursively and iteratively.
  */
 
 /**
@@ -70,7 +100,7 @@ public class SameTree {
         if (p.val != q.val) {
             return false;
         }
-        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
 
