6a06deeaa0ff9eb90c1cacbadf4babfc7aa644a6
Maksym Rohachov <romax982@gmail.com>
2022-12-13 00:18:22 +0100

    Update description for algorithms

--- src/main/java/com/task/algorithm/Easy/solved/ReverseALinkedList.java
+++ src/main/java/com/task/algorithm/Easy/solved/ReverseALinkedList.java
@@ -1,37 +1,33 @@
 package com.task.algorithm.Easy.solved;
 
-// Java program for reversing linked list using additional space
-
 /*
-    206. Reverse Linked List
-
-    Given the head of a singly linked list, reverse the list,
-    and return the reversed list.
-
-    Example 1:
-
-    Input: head = [1,2,3,4,5]
-    Output: [5,4,3,2,1]
-
-    Example 2:
-
-    Input: head = [1,2]
-    Output: [2,1]
-
-    Example 3:
-
-    Input: head = []
-    Output: []
-
-    Constraints:
-
-    * The number of nodes in the list is the range
-    [0, 5000].
-    * -5000 <= Node.val <= 5000
-
-
-    Follow up: A linked list can be reversed either
-    iteratively or recursively. Could you implement both?
+ * 206. Reverse Linked List
+ * https://leetcode.com/problems/reverse-linked-list/
+ *
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ *
+ * Example 1:
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ *
+ * Example 2:
+ *
+ * Input: head = [1,2]
+ * Output: [2,1]
+ *
+ * Example 3:
+ *
+ * Input: head = []
+ * Output: []
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is the range [0, 5000].
+ * -5000 <= Node.val <= 5000
+ *
+ * Follow up: A linked list can be reversed either iteratively or recursively. Could you implement
+ * both?
  */
 
 /**
@@ -45,6 +41,7 @@ package com.task.algorithm.Easy.solved;
  * }
  */
 
+// Java program for reversing linked list using additional space
 // Definition for singly-linked list.
 class ListNode {
     int val;
