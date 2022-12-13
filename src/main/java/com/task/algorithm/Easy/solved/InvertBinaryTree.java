9d211300e4e80124ffb2cf11b56a2fee669644ac
Maksym Rohachov <romax982@gmail.com>
2022-12-13 01:02:04 +0100

    Update description for algorithms

--- src/main/java/com/task/algorithm/Easy/solved/InvertBinaryTree.java
+++ src/main/java/com/task/algorithm/Easy/solved/InvertBinaryTree.java
@@ -26,7 +26,6 @@ package com.task.algorithm.Easy.solved;
  * The number of nodes in the tree is in the range [0, 100].
  * -100 <= Node.val <= 100
  *
- *
  * An alternative algorithm description:
  * -------------------------------------
  * An inversion, or mirror, of a Binary Tree (T), is just a Binary Tree M(T) whose
@@ -41,7 +40,6 @@ package com.task.algorithm.Easy.solved;
  */
 
 // https://www.techiedelight.com/invert-binary-tree-recursive-iterative/
-// https://leetcode.com/problems/invert-binary-tree/
 // https://www.educative.io/answers/how-to-invert-a-binary-tree
 // https://www.baeldung.com/java-reversing-a-binary-tree
 
@@ -62,31 +60,29 @@ package com.task.algorithm.Easy.solved;
  */
 
 // Definition for a binary tree node.
-class InvertFlipBinaryTree {
+class InvertBinaryTree {
     int val;
-    InvertFlipBinaryTree left;
-    InvertFlipBinaryTree right;
+    InvertBinaryTree left;
+    InvertBinaryTree right;
 
-    InvertFlipBinaryTree() {
-    }
+    InvertBinaryTree() { }
 
-    InvertFlipBinaryTree(int val) {
+    InvertBinaryTree(int val) {
         this.val = val;
     }
 
-    InvertFlipBinaryTree(int val, InvertFlipBinaryTree left, InvertFlipBinaryTree right) {
+    InvertBinaryTree(int val, InvertBinaryTree left, InvertBinaryTree right) {
         this.val = val;
         this.left = left;
         this.right = right;
     }
 
-
     public class Solution {
-        public InvertFlipBinaryTree invertTree(InvertFlipBinaryTree root) {
+        public InvertBinaryTree invertTree(InvertBinaryTree root) {
             if (root == null || (root.left == null && root.right == null)) return root;
 
             // reversing left and right cotyledons
-            InvertFlipBinaryTree tmp = root.left;
+            InvertBinaryTree tmp = root.left;
             root.left = root.right;
             root.right = tmp;
 
