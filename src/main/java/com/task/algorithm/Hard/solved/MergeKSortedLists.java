7f22c77c15ed5408f181dbde743113969b93f89d
Maksym Rohachov <romax982@gmail.com>
2022-12-23 04:28:10 +0100

    Update the logic of the project

--- src/main/java/com/task/algorithm/Hard/solved/MergeKSortedLists.java
+++ src/main/java/com/task/algorithm/Hard/solved/MergeKSortedLists.java
@@ -1,4 +1,4 @@
-package com.task.algorithm.Hard;
+package com.task.algorithm.Hard.solved;
 
 /*
  * 23. Merge k Sorted Lists
@@ -40,7 +40,62 @@ package com.task.algorithm.Hard;
  * lists[i] is sorted in ascending order.
  * The sum of lists[i].length will not exceed 10^4.
  */
-public class Solution {
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode empty = null;
+        if (lists.length == 0)
+            return empty;
+        ListNode temp = lists[0];
+        for (int i = 1; i < lists.length; i++) {
+            temp = mergeklists(temp, lists[i]);
+        }
+        return temp;
+    }
+
+    public ListNode mergeklists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+
+        if (l1.val < l2.val) {
+            l1.next = mergeklists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeklists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+/*public class Solution {
     public static class ListNode {
         int val;
         ListNode next;
@@ -93,7 +148,7 @@ public class Solution {
         }
         return dummy.next;
     }
-}
+}*/
 
 /*class MergeKSortedLists {
 
---
+++ src/main/java/com/task/algorithm/Medium/solved/WordDictionary.java
@@ -0,0 +1,114 @@
+package com.task.algorithm.Medium.solved;
+
+import java.util.HashMap;
+
+public class WordDictionary {
+    static class TrieNodeWithMap {
+        boolean isEndOfWord;
+        HashMap<Character, TrieNodeWithMap> trieNodeMap;
+
+        TrieNodeWithMap() {
+            this.isEndOfWord = false;
+            this.trieNodeMap = new HashMap<>();
+        }
+    }
+
+    private final TrieNodeWithMap root;
+
+    public WordDictionary() {
+        this.root = new TrieNodeWithMap();
+    }
+
+    public void addWord(String word) {
+        TrieNodeWithMap node = root;
+        for (int index = 0; index < word.length(); index++) {
+            char character = word.charAt(index);
+            if (!node.trieNodeMap.containsKey(character)) node.trieNodeMap.put(character, new TrieNodeWithMap());
+            node = node.trieNodeMap.get(character);
+        }
+        node.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        return searchRec(word, root, 0);
+    }
+
+    private boolean searchRec(String word, TrieNodeWithMap node, int index) {
+        if (node == null) return false;
+        if (index == word.length()) return node.isEndOfWord;
+        char character = word.charAt(index);
+        if (character == '.') {
+            for (Character ch : node.trieNodeMap.keySet()) {
+                boolean isFound = searchRec(word, node.trieNodeMap.get(ch), index + 1);
+                if (isFound) return true;
+            }
+        } else if (!node.trieNodeMap.containsKey(character)) return false;
+        return searchRec(word, node.trieNodeMap.get(character), index + 1);
+    }
+
+    public static void main(String[] args) {
+        WordDictionary wordDictionary = new WordDictionary();
+        wordDictionary.addWord("a");
+        wordDictionary.addWord("a");
+        System.out.println(wordDictionary.search("."));
+        System.out.println(wordDictionary.search("a"));
+        System.out.println(wordDictionary.search("aa"));
+        System.out.println(wordDictionary.search("a"));
+        System.out.println(wordDictionary.search(".a"));
+        System.out.println(wordDictionary.search("a."));
+    }
+}
+
+/*
+import java.util.*;
+import java.util.regex.Pattern;
+
+class WordDictionary {
+    ArrayList<String> arr;
+    HashMap<String, Integer> map;
+
+    public WordDictionary() {
+        arr = new ArrayList<>();
+        map = new HashMap<>();
+    }
+
+    void addWord(String x) {
+        if (map.get(x) != null)
+            return;
+
+        int s = arr.size();
+        arr.add(x);
+        map.put(x, s);
+    }
+
+    void search(String x) {
+        if (map.get(x) != null) {
+            System.out.println("True");
+            return;
+        } else {
+            Pattern regex = Pattern.compile(x);
+
+            for (String s : arr) {
+                if (regex.matcher(s).matches()) {
+                    System.out.println("True");
+                    return;
+                }
+            }
+        }
+        System.out.println("False");
+    }
+}
+
+class Main2 {
+    public static void main(String[] args) {
+        WordDictionary ds = new WordDictionary();
+
+        ds.addWord("code");
+        ds.addWord("java");
+        ds.addWord("when");
+
+        ds.search("blue");
+        ds.search("java");
+        ds.search("co..");
+    }
+}*/
