2653592b3617eca9c1f12799effe162c6f2a6c4a
Maksym Rohachov <romax982@gmail.com>
2022-12-24 21:22:27 +0100

    Update the algorithm

--- src/main/java/com/task/algorithm/Medium/FindMinimumInRotatedSortedArray.java
+++ src/main/java/com/task/algorithm/Medium/FindMinimumInRotatedSortedArray.java
@@ -1,6 +1,89 @@
 package com.task.algorithm.Medium;
 
 public class FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[left] < nums[mid]) {
+                if (nums[mid] <= nums[right]) {
+                    // all increasing, on left half
+                    right = mid - 1;
+                } else {
+                    // up and down, on right half
+                    left = mid + 1;
+                }
+            } else if (nums[left] > nums[mid]) {
+                // must be on left half
+                left++;
+                right = mid;
+            } else {
+                // duplicates
+                if (left == mid) {
+                    return Math.min(nums[left], nums[right]);
+                } else {
+                    left++;
+                }
+            }
+        }
+        return nums[left];
+    }
+    public static void main(String[] args) {
+        FindMinimumInRotatedSortedArray s = new FindMinimumInRotatedSortedArray();
+        System.out.format("%d\n", s.findMin(new int[] {4, 5, 6, 7, 0, 1, 2}));
+        System.out.format("%d\n", s.findMin(new int[] {2, 1}));
+        System.out.format("%d\n", s.findMin(new int[] {3, 1, 2}));
+        System.out.format("%d\n", s.findMin(new int[] {4, 5, 1, 2, 3}));
+        System.out.format("%d\n", s.findMin(new int[] {3, 3, 1, 3}));
+        System.out.format("%d\n", s.findMin(new int[] {1, 3}));
+        System.out.format("%d\n", s.findMin(new int[] {1, 3, 3}));
+    }
+}
+
+/*public class FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[left] < nums[mid]) {
+                if (nums[mid] <= nums[right]) {
+                    // all increasing, on left half
+                    right = mid - 1;
+                } else {
+                    // up and down, on right half
+                    left = mid + 1;
+                }
+            } else if (nums[left] > nums[mid]) {
+                // must be on left half
+                left++;
+                right = mid;
+            } else {
+                // duplicates
+                if (left == mid) {
+                    return Math.min(nums[left], nums[right]);
+                } else {
+                    left++;
+                }
+            }
+        }
+        return nums[left];
+    }
+    public static void main(String[] args) {
+        FindMinimumInRotatedSortedArray s = new FindMinimumInRotatedSortedArray();
+        System.out.format("%d\n", s.findMin(new int[] {4, 5, 6, 7, 0, 1, 2}));
+        System.out.format("%d\n", s.findMin(new int[] {2, 1}));
+        System.out.format("%d\n", s.findMin(new int[] {3, 1, 2}));
+        System.out.format("%d\n", s.findMin(new int[] {4, 5, 1, 2, 3}));
+        System.out.format("%d\n", s.findMin(new int[] {3, 3, 1, 3}));
+        System.out.format("%d\n", s.findMin(new int[] {1, 3}));
+        System.out.format("%d\n", s.findMin(new int[] {1, 3, 3}));
+    }
+}*/
+
+/*
+public class FindMinimumInRotatedSortedArray {
 
     // Function to find the minimum value
     static int findMin(int[] arr, int n) {
@@ -22,4 +105,4 @@ public class FindMinimumInRotatedSortedArray {
         int N = arr.length;
         System.out.println(findMin(arr, N));
     }
-}
+}*/
