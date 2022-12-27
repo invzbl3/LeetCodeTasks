d7c7e9a2f46eb50e25ae56df0c40eae9343e6ddd
Maksym Rohachov <romax982@gmail.com>
2022-12-27 22:52:25 +0100

    Update the code

--- src/main/java/com/task/algorithm/Hard/solved/AlienDictionary.java
+++ src/main/java/com/task/algorithm/Hard/solved/AlienDictionary.java
@@ -1,7 +1,110 @@
 package com.task.algorithm.Hard.solved;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+/**
+ * 269. Alien Dictionary
+ * https://leetcode.com/problems/alien-dictionary/
+ *
+ * There is a new alien language that uses the English alphabet.
+ * However, the order among the letters is unknown to you.
+ *
+ * You are given a list of string words from the alien language's dictionary,
+ * where the strings in words are sorted lexicographically by the rules of this
+ * new language.
+ *
+ * Return a string of the unique letters in the new alien language sorted in
+ * lexicographically increasing order by the new language's rules, if there is
+ * no solution, return == if there are multiple solutions, return any of them.
+ *
+ * A string s is lexicographically smaller than a string t if at the first letter
+ * where they differ, the letter in s comes before the letter in t in the alien
+ * language. If the first min(s.length, t.length) letters are the same, then
+ * s is smaller if and only if s.length < t.length.
+ *
+ * Example 1:
+ *
+ * Input: words = ["wrt", "wrf", "er", "ett", "rftt"]
+ * Output: "wertf"
+ *
+ * Example 2:
+ *
+ * Input: words = ["z", "x"]
+ * Output: "zx"
+ *
+ * Example 3:
+ *
+ * Input: words = ["z", "x", "z"]
+ * Output: ""
+ * Explanation: The order is invalid, so return "".
+ */
+
+class AlienDictionary {
+
+    public String alienOrder(String[] words) {
+        Map<Character, Set<Character>> map = new HashMap<>();
+        int[] inDegree = new int[26];
+
+
+        buildMap(words, map, inDegree);
+        // dfs to get order
+        return dfs(map, inDegree);
+    }
+
+    public void buildMap(String[] words, Map<Character, Set<Character>> map, int[] inDegree) {
+        for (String s : words) {
+            for (char c : s.toCharArray()) {
+                map.putIfAbsent(c, new HashSet<>());
+            }
+        }
+
+        for (int i = 1; i < words.length; i++) {
+            String first = words[i - 1];
+            String second = words[i];
+
+            if (first.length() > second.length() && first.startsWith(second)) {
+                map.clear();
+                return;
+            }
+
+            // edge case
+            for (int j = 0; j < first.length(); j++) {
+                char key = first.charAt(j);
+                char value = second.charAt(j);
+                if (key != value) {
+                    if (!map.get(key).contains(value)) {
+                        inDegree[value - 'a']++;
+                        map.get(key).add(value);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    public String dfs(Map<Character, Set<Character>> map, int[] inDegree) {
+        StringBuilder sb = new StringBuilder();
+        Queue<Character> queue = new LinkedList<>();
+
+        for (char c : map) {
+            if (inDegree[c - 'a' == 0]) {
+                queue.offer(c);
+                sb.append(c);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            char c = queue.poll();
+            for (char nextC : map.get(c)) {
+                if (inDegree[nextC - 'a'] == 0) {
+                    queue.offer(nextC);
+                    sb.append(nextC);
+                }
+            }
+        }
+        return sb.length() == map.size() ? sb.toString() : "";
+    }
+}
 
 /*
  *   Algorithm description:
