80219a1ec535a6582dc26353924dc2e0d2d4624e
Maksym Rohachov <romax982@gmail.com>
2023-01-07 16:21:21 +0100

    Update the project logic

--- src/main/java/com/task/algorithm/Easy/solved/DetectCycleInALinkedList.java
+++ src/main/java/com/task/algorithm/Easy/solved/DetectCycleInALinkedList.java
@@ -139,6 +139,8 @@ class DetectCycleInALinkedList { */
      * Output Format
      *
      * If the list contains a cycle, your function must return true. If the list does not contain a cycle,
+     * it must return false. The binary integer corresponding to the boolean value returned by your
+     * function is printed to stdout by our hidden code checker.
      */
 
 /*
